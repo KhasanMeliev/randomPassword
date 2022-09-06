18d4566a39a3a949d9c58894f702fa1ba40018a1
melievkh <khushnudmeliev21@gmail.com>
2022-09-06 16:12:59 +0500

    changed code in html and js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-var password = document.getElementById("password")
+var password = document.getElementById("password");
 
 function generatorPassword() {
-    var chars = '0123456789abcdefghijklmnopqrstuvxyz'
-    var passwordLength = 10;
-    var password = ''
-    for (var i = 0; i < passwordLength; i++){
-        var randomNumber = Math.floor(Math.random() * chars.length)
-        password += chars.substring(randomNumber, randomNumber+1)
-    }
-    document.getElementById("password").value = password
+  var chars = "0123456789abcdefghijklmnopqrstuvxyz";
+  var passwordLength = 10;
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
 }
-
 function copyPassword() {
-    var copyText = document.getElementById('password')
-    copyText.select()
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand('copy')
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 }
